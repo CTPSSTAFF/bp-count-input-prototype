a2ad63f18974646195f6715294af06086cf4c644
Benjamin Krepp <bkrepp@ctps.org>
2023-10-03 11:14:16 -0400

    'Hello, world' example, from website

--- js/main.js
+++ js/main.js
@@ -185,16 +185,15 @@ function initialize() {
 	];
 	
 
-	// Create grid for input of count data itself
-    $("#jsGrid").jsGrid({
-        width: "100%",
-        height: "600px",
-		pageSize: 12,
-        inserting: true,
-        editing: true,
-        sorting: false,
-        paging: true,
-        data: empty_data,
-        fields: columns
-    });
+	// Minimal "hello, world" test
+	var grid = new gridjs.Grid({
+	  columns: ["Name", "Email", "Phone Number"],
+	  data: [
+		["John", "john@example.com", "(353) 01 222 3333"],
+		["Mark", "mark@gmail.com", "(01) 22 888 4444"],
+		["Eoin", "eoin@gmail.com", "0097 22 654 00033"],
+		["Sarah", "sarahcdd@gmail.com", "+322 876 1233"],
+		["Afshin", "afshin@mail.com", "(353) 22 87 8356"]
+	  ]
+	}).render(document.getElementById("wrapper"));
 } // initialize
